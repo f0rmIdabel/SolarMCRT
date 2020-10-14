7e4fe3ea79bd790f3d7a3b856da526b0a54be078
f0rmIdabel <54349024+f0rmIdabel@users.noreply.github.com>
2020-10-14 14:41:54 +0200

    Restructuring. Complete chaos.

---
+++ src/atmos.jl
@@ -0,0 +1,75 @@
+using HDF5
+using Unitful
+import PhysicalConstants.CODATA2018: σ_e
+
+struct Atmosphere
+   # Dimensions
+   x::Array{<:Unitful.Length, 1}
+   y::Array{<:Unitful.Length, 1}
+   z::Array{<:Unitful.Length, 1}
+
+   # Local properties
+   velocity_x::Array{<:Unitful.Velocity, 3}
+   velocity_y::Array{<:Unitful.Velocity, 3}
+   velocity_z::Array{<:Unitful.Velocity, 3}
+   temperature::Array{<:Unitful.Temperature, 3}
+   χ_continuum::Array{<:Unitful.Quantity{<:Real, Unitful.𝐋^(-1)}, 3}
+   ε_continuum::Array{Real, 3}
+end
+
+"""
+   function get_atmosphere_data(atmos_data,
+                                rh_output)
+
+Reads atmosphere parameters and reworks them to fit simulation.
+"""
+function get_atmosphere_data(atmos_data,
+                             rh_output)
+
+    path_atmos = "../../basement/MScProject/Atmospheres/"*atmos_data
+    path_RH = "../../basement/MScProject/Atmospheres/"*rh_output
+
+    # ===========================================================
+    # READ ATMOSPHERE FILE
+    # ===========================================================
+    x = h5read(path_atmos, "x")u"m"
+    y = -h5read(path_atmos, "y")u"m"
+    z = h5read(path_atmos, "z")[:,1]u"m"
+
+    velocity_x = h5read(path_atmos, "velocity_x")[:,:,:,1]u"m/s"
+    velocity_y = h5read(path_atmos, "velocity_y")[:,:,:,1]u"m/s"
+    velocity_z = h5read(path_atmos, "velocity_z")[:,:,:,1]u"m/s"
+
+    temperature = h5read(path_atmos, "temperature")[:,:,:,1]u"K"
+    electron_density = h5read(path_atmos, "electron_density")[:,:,:,1]u"m^-3"
+
+    # ===========================================================
+    # READ RH OUTPUT
+    # ===========================================================
+    χ_absorption = h5read(path_RH, "chi_continuum")[1,:,:,:]u"m^-1"  #WL
+    # ===========================================================
+    # RE-WORK PARAMETERS
+    # ===========================================================
+    # Add endpoints for box calculations
+    x = push!(x, 2*x[end] - x[end-1])
+    y = push!(y, 2*y[end] - y[end-1])
+    z = push!(z, 2*z[end] - z[end-1])
+
+    # Calculate epsilon and chi                                       #WL
+    χ_thomson = σ_e*electron_density
+    χ_continuum = χ_absorption .+ χ_thomson
+    ε_continuum = χ_absorption ./ χ_continuum
+
+    # Transpose all 3D-space arrays,(k,i,j) -> (i,j,k)
+    velocity_x = permutedims(velocity_x, [2,3,1])
+    velocity_y = permutedims(velocity_y, [2,3,1])
+    velocity_z = permutedims(velocity_z, [2,3,1])
+    temperature = permutedims(temperature, [2,3,1])
+    χ_continuum = permutedims(χ_continuum, [2,3,1])
+    ε_continuum = permutedims(ε_continuum, [2,3,1])
+
+    return x, y, z, velocity_x, velocity_y, velocity_z,
+           temperature, χ_continuum, ε_continuum
+end
+
+#
---
+++ src/physLib.jl
@@ -0,0 +1,213 @@
+using Unitful
+import PhysicalConstants.CODATA2018: c_0, h, k_B
+
+"""
+    function blackbody_lambda(λ::Unitful.Length,
+                              temperature::Unitful.Temperature)
+
+Calculates the Blackbody (Planck) function per wavelength, for given
+arrays of wavelength and temperature. Returns monochromatic intensity.
+
+Copied from Tiago, SSB
+"""
+function blackbody_lambda(λ::Unitful.Length,
+                          temperature::Unitful.Temperature)
+    (2h * c_0^2) / ( λ^5 * (exp((h * c_0 / k_B) / (λ * temperature)) - 1) ) |> u"kW / m^2 / sr / nm"
+end
+
+"""
+    function optical_depth_boundary(χ::Array{<:Unitful.Quantity{<:Real, Unitful.𝐋^(-1)}, 3},
+                                    z::Array{<:Unitful.Length, 1},
+                                    τ_max::Real)
+
+Returns 2D array containing the k-indices where the optical depth reaches τ_max.
+"""
+function optical_depth_boundary(χ::Array{<:Unitful.Quantity{<:Real, Unitful.𝐋^(-1)}, 3},
+                                z::Array{<:Unitful.Length, 1},
+                                τ_max::Real)
+
+    nx, ny, nz = size(χ)
+    columns = nx*ny
+    boundary = Array{Int, 2}(undef, nx, ny)
+
+    # Calculate vertical optical depth for each column
+    Threads.@threads for col=1:columns
+        i = 1 + (col-1)÷ny
+        j = col - (i-1)*ny
+
+        τ = 0
+        k = 0
+
+        while τ < τ_max && k < ny
+            k += 1
+            # Trapezoidal rule
+            τ += 0.5(z[k] - z[k+1]) * (χ[i,j,k] + χ[i,j,k+1])
+        end
+        boundary[i,j] = k
+    end
+
+    return boundary
+end
+
+"""
+    function total_emission(χ::Array{<:Unitful.Quantity{<:Real, Unitful.𝐋^(-1)}, 3},
+                        temperature::Array{<:Unitful.Temperature, 3},
+                        x::Array{<:Unitful.Length, 1},
+                        y::Threads.@threads Array{<:Unitful.Length, 1},
+                        z::Array{<:Unitful.Length, 1},
+                        boundary::Array{Int,2},
+                        λ::Unitful.Length)
+
+Reurns the total emission above the optical depth boundary in intensity units.
+"""
+function total_emission(χ::Array{<:Unitful.Quantity{<:Real, Unitful.𝐋^(-1)}, 3},
+                        temperature::Array{<:Unitful.Temperature, 3},
+                        x::Array{<:Unitful.Length, 1},
+                        y::Array{<:Unitful.Length, 1},
+                        z::Array{<:Unitful.Length, 1},
+                        boundary::Array{Int,2},
+                        λ::Unitful.Length)
+
+    total_emission = 0.0u"kW / sr / nm"
+
+    nx,ny = size(boundary)
+    columns = nx*ny
+
+    # Calculate vertical optical depth for each column
+    for col=1:columns
+
+        i = 1 + (col-1)÷ny
+        j = col - (i-1)*ny
+
+        for k=1:boundary[i,j]
+            box_volume = (x[i+1] - x[i])*(y[j+1] - y[j])*(z[k] - z[k+1])
+            B = blackbody_lambda(λ, temperature[i,j,k])
+            total_emission += B*χ[i,j,k]*box_volume
+        end
+    end
+
+    return total_emission
+end
+
+"""
+    function extract_surface_bin(surface::Array{Int, 4},
+                                 bin = :[:,:])
+
+Slices the surface for the given escape bins of ϕ and θ.
+"""
+function extract_surface_bin(surface::Array{Int, 4},
+                             bin = :[:,:])
+
+    # Pick out bin
+    surface = surface[:,:, eval(bin)[1], eval(bin)[2]]
+    dim = length(size(surface))
+
+    # If no bin specified
+    if dim == 4
+        surface = sum(surface, dims = 4)
+        surface = surface[:,:,:,1]
+    end
+
+    # If less than one bin specified
+    if dim == 4 || dim == 3
+        surface = sum(surface, dims=3)
+        surface = surface[:,:,1]
+    end
+
+    return surface
+end
+
+"""
+    function field_above_boundary(z::Array{<:Unitful.Length, 1},
+                                  χ::Array{<:Unitful.Quantity{<:Real, Unitful.𝐋^(-1)}, 3},
+                                  J::Array{Int, 3},
+                                  τ_max::Real)
+
+Calculates the average, maximum and minimum intensity value of the field above the boundary.
+"""
+function field_above_boundary(z::Array{<:Unitful.Length, 1},
+                              χ::Array{<:Unitful.Quantity{<:Real, Unitful.𝐋^(-1)}, 3},
+                              J::Array{Int, 3},
+                              τ_max::Real)
+
+    boundary = optical_depth_boundary(χ, z, τ_max)
+
+    nx, ny = size(boundary)
+    columns = nx*ny
+
+    # Initialize variables
+    meanJ = 0.0
+    minJ =  J[1,1,1]
+    maxJ =  J[1,1,1]
+
+    total_boxes = 0
+
+    # Calculate mean, min and max J above boundary
+    for col=1:columns
+
+        i = 1 + (col - 1)÷ny
+        j = col - (i - 1)*ny
+
+        for k=1:boundary[i,j]
+            meanJ += J[i,j,k]
+            total_boxes += 1
+
+            if J[i,j,k] > maxJ
+                maxJ = J[i,j,k]
+            elseif J[i,j,k] < minJ
+                minJ = J[i,j,k]
+            end
+        end
+    end
+
+    meanJ = meanJ/total_boxes
+    return meanJ, minJ, maxJ
+end
+
+"""
+    packets_per_box(x::Array{<:Unitful.Length, 1},
+                         y::Array{<:Unitful.Length, 1},
+                         z::Array{<:Unitful.Length, 1},
+                         χ::Array{<:Unitful.Quantity{<:Real, Unitful.𝐋^(-1)}, 3},
+                         temperature::Array{<:Unitful.Temperature, 3},
+                         λ::Unitful.Length,
+                         target_packets::Real,
+                         boundary::Array{Int64,2})
+
+Returns a 3D array of the # of packets to be generated in each box.
+"""
+function packets_per_box(x::Array{<:Unitful.Length, 1},
+                         y::Array{<:Unitful.Length, 1},
+                         z::Array{<:Unitful.Length, 1},
+                         χ::Array{<:Unitful.Quantity{<:Real, Unitful.𝐋^(-1)}, 3},
+                         temperature::Array{<:Unitful.Temperature, 3},
+                         λ::Unitful.Length,
+                         target_packets::Real,
+                         boundary::Array{Int64,2})
+
+    nx,ny = size(boundary)
+    nz = maximum(boundary)
+    total_boxes = nx*ny*nz
+
+    total_emission_ = total_emission(χ, temperature, x, y, z, boundary, λ)
+    scale_emission = target_packets/total_emission_
+
+    packets = zeros(Int64,nx,ny,nz)
+
+    for box=1:total_boxes
+        i = 1 + (box-1) ÷ (ny*nz)
+        j = 1 + (box - (i-1)*ny*nz - 1) ÷ nz
+        k = 1 + (box - (i-1)*ny*nz - 1) % nz
+
+        # Skip boxes beneath boundary
+        if k > boundary[i,j]
+            continue
+        end
+
+        B = blackbody_lambda(λ, temperature[i,j,k])
+        box_volume = (x[i+1] - x[i])*(y[j+1] - y[j])*(z[k] - z[k+1])
+        box_emission = B*χ[i,j,k]*box_volume
+        packets[i,j,k] = Int(round(box_emission*scale_emission))
+    end
+    return packets
+end
